3e2e951a818f9048c9cda894d8ecdbc94026f7e3
Andron-S <slepok.andron@yandex.ru>
2023-03-03 20:40:04 +0300

    Создал таблицы в базе данных

---
+++ api/core/models.py
@@ -0,0 +1,25 @@
+import datetime
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
+
+
+class Genre(models.Model):
+    title = models.CharField(max_length=32, blank=False, null=False, unique=True)
+
+
+class Book(models.Model):
+    genre = models.ForeignKey(to=Genre, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(to=Author, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=100, blank=False, null=False, unique=True)
+    description = models.TextField(blank=True, null=True)
+    content = models.TextField(blank=False, null=False)
+
+
+class UserBook(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    book = models.ForeignKey(to=Book, on_delete=models.CASCADE)
+    date_added = models.DateField(default=datetime.date.today)
